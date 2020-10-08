7668cede3297c983e9a8db9e712aa78d49ebfa56
subhadeepchandra1 <subhadeepchandra1@gmail.com>
2020-10-08 23:43:12 +0530

    White Noise

--- Post_Covid/settings.py
+++ Post_Covid/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +136,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 # Custom User Model
 AUTH_USER_MODEL = "Profiles.CustomUser"
