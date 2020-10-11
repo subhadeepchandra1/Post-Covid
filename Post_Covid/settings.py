66c6f7704407d30260a6a5451b5a60691940e094
subhadeepchandra1 <subhadeepchandra1@gmail.com>
2020-10-11 20:57:56 +0530

    CORS enabled

--- Post_Covid/settings.py
+++ Post_Covid/settings.py
@@ -53,11 +53,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
-
     'django.middleware.security.SecurityMiddleware',
     
     'whitenoise.middleware.WhiteNoiseMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
